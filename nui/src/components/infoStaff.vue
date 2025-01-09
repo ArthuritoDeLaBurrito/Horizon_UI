03b93dd0b460a54b0e718c3fbc0f084771241f97
ArthuritoDeLaBurrito <inchallahjedev@gmail.com>
2025-01-09 22:03:08 +0000

    Enhance infoStaff component with detailed staff information and real-time clock

--- nui/src/components/infoStaff.vue
+++ nui/src/components/infoStaff.vue
@@ -1,6 +1,13 @@
 <template>
     <div v-if="visible" class="info-container">
-      <p>Les mouvements de caméra et les coups de poing sont désactivés.</p>
+      <p class="info_title">Information STAFF</p>
+      <br>
+      <p class="info_text">Reports en attente: {{ rClaim }}</p>
+      <p class="info_text">Total des Reports: {{ rTotal }}</p>
+      <p class="info_text">Joueurs en ligne: {{ pOnline }}</p>
+      <p class="info_text">Staff en ligne: {{ sOnline }}</p>
+      <br>
+      <p class="info_text">Heure : {{ time }}</p>
     </div>
 </template>
 
@@ -12,6 +19,7 @@ const rTotal = ref(0);
 const pOnline = ref(0);
 const sOnline = ref(0);
 const visible = ref(false); // État de visibilité du HUD
+const time = ref("00:00");
 
 // Écouter les messages NUI
 onMounted(() => {
@@ -25,20 +33,29 @@ onMounted(() => {
       visible.value = event.data.display; // Affiche ou masque le HUD
     }
   });
+  updateTime(); // Met à jour immédiatement
+  setInterval(updateTime, 1000); // Met à jour toutes les secondes
 });
 
+function updateTime() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  time.value = `${hours}:${minutes}`;
+}
 </script>
 
 <style scoped>
+ 
   .info-container {
     position: fixed;
-    left: 20px;
-    top: 20px;
+    right: 20px;
+    top: 50%;
     z-index: 1000;
     display: flex;
     flex-direction: column;
     gap: 15px;
-    color: rgb(0, 0, 0);
+    color: rgb(255, 255, 255);
     font-family: Arial, sans-serif;
     background: rgba(0, 0, 0, 0.5);
     padding: 10px;
@@ -46,7 +63,21 @@ onMounted(() => {
     width: 250px;
     animation: fadeIn 0.5s ease-in-out;
   }
-  
+
+  .info_title {
+    font-size: 20px;
+    font-weight: bold;
+    color: rgb(255, 255, 255);
+    text-align: center;
+  }
+
+  .info_text {
+    font-size: 16px;
+    font-weight: bold;
+    color: rgb(255, 255, 255);
+    text-align: center;
+  }
+
   @keyframes fadeIn {
   0% {
     opacity: 0;
